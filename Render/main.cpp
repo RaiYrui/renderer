2ae7c5567a8abb82bb8e9f1ea4b27a30f2343a6d
容易 <pb.adcycry001@mesg.corp.netease.com>
2024-07-15 09:58:46 +0800

    wdsadwds

--- Render/main.cpp
+++ Render/main.cpp
@@ -20,7 +20,7 @@ int main()
 	if (app->Init())
 		app->Start();
 	else 
-		cout << "Init Failed" << endl;
+		cout << "Init Faihrfnfjksndjkfled" << endl;
 	cout << "Hello CMake." << endl;
 	return 0;
 }
